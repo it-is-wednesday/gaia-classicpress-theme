2b25054e55d9818351bf6d4031c1dc450b0d7a71
Maor Kadosh <maork@venn.city>
2022-11-28 18:16:57 +0200

    prettify front page :^)

---
+++ src/front-page.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * I will not let Gaia touch <!--more-->
+ */
+function the_content_trimmed()
+{
+    $content = wp_trim_words(get_the_content(), 60, "...");
+    $content = apply_filters("the_content", $content);
+    $content = str_replace("]]>", "]]&gt;", $content);
+    return $content;
+}
+get_header();
+?>
+<div dir="rtl">
+    <?php if (have_posts()): ?>
+
+    <?php if (is_home() && !is_front_page()): ?>
+        <header>
+            <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+        </header>
+    <?php endif; ?>
+
+    <?php while (have_posts()): ?>
+        <?php the_post(); ?>
+        <?= the_content_trimmed() ?>
+        <a href="<?= get_post_permalink() ?>">
+            <h1><?= get_the_title() ?></h1>
+        </a>
+    <?php endwhile; ?>
+
+    <?php // Previous/next page navigation.
+
+        the_posts_pagination([
+        "prev_text" => __("Previous page", "twentyfifteen"),
+        "next_text" => __("Next page", "twentyfifteen"),
+        "before_page_number" =>
+            '<span class="meta-nav screen-reader-text">' .
+            __("Page", "twentyfifteen") .
+            " </span>",
+    ]); ?>
+
+    <?php else: ?>
+        <?php get_template_part("content", "none"); ?>
+    <?php endif; ?>
+</div>
+<?php get_footer(); ?>
